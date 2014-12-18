bf3524fe73f44df66a8291713a4a70c73d7aab61
Phil Rosenberg <philip_rosenberg@yahoo.com>
2014-12-18 13:24:06 +0000

    Removed virtual functions from Link. Replaced update with a function pointer and ensured that appropriate variables are set in update such that getLinkProperties no longer requires to be virtual. This reduced runtime of test 6 from 67 s to 46 s (30% performance boost). Note that just removing virtual from getLink properties gave a run time of 62 s, so no large boost. This increase seems to be because the Link class is entirely inlined and optimised away. It is a shame the compiler does not seem able to inline virtual functions in the same way. Note also that I tried using templates, (see the dead end templates branch) which gave the same performance boost, but made building simulations very very difficult. This method seems to be a happy medium.

--- ung/link.cpp
+++ ung/link.cpp
@@ -2,8 +2,23 @@
 #include"cell.h"
 #include<memory>
 
-Link::Link(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2)
-	:m_cell1(cell1), m_cell2(cell2), m_area(area), m_speed1(speed1), m_speed2(speed2)
+void inviscidLinkUpdate( void *linkVoid, UNG_FLT timeStep)
+{
+	InviscidLink *link=(InviscidLink*)linkVoid;
+	UNG_FLT pressureDif=(link->m_cell2->getPressure()-link->m_cell1->getPressure());
+	UNG_FLT pressureImpulse=-link->m_area*pressureDif*timeStep/UNG_FLT(2.0);
+	link->m_parallelImpulse=pressureImpulse;
+	UNG_FLT transferSpeed=(*link->m_speed1+*link->m_speed2)/UNG_FLT(2.0);
+	UNG_FLT transferDistance=transferSpeed*timeStep;
+	link->m_transferVolume=transferDistance*link->m_area;
+	link->m_cell1->averageTracerDensities(link->m_tracerDensities, link->m_cell1->getTracerDensities(), link->m_cell2->getTracerDensities());
+}
+void solidBoundaryLinkUpdate( void *solidBoundaryLink, UNG_FLT timestep)
+{
+}
+
+Link::Link(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2, void (*updateFunc)( void *, UNG_FLT ))
+	:m_cell1(cell1), m_cell2(cell2), m_area(area), m_speed1(speed1), m_speed2(speed2), m_updateFunc(updateFunc)
 {
 	m_tracerDensities=(void*)new char[cell1->getTracerDensitySize()];
 }
@@ -17,25 +32,31 @@ Link::~Link()
 //values when getLinkProperties is called
 void Link::update(UNG_FLT timeStep)
 {
-	UNG_FLT pressureDif=(m_cell2->getPressure()-m_cell1->getPressure());
-	m_pressureImpulse=-m_area*pressureDif*timeStep/UNG_FLT(2.0);
+	m_updateFunc(this, timeStep);
+	/*UNG_FLT pressureDif=(m_cell2->getPressure()-m_cell1->getPressure());
+	UNG_FLT pressureImpulse=-m_area*pressureDif*timeStep/UNG_FLT(2.0);
+	m_parallelImpulse=pressureImpulse;
 	UNG_FLT transferSpeed=(*m_speed1+*m_speed2)/UNG_FLT(2.0);
 	UNG_FLT transferDistance=transferSpeed*timeStep;
 	m_transferVolume=transferDistance*m_area;
-	m_cell1->averageTracerDensities(m_tracerDensities, m_cell1->getTracerDensities(), m_cell2->getTracerDensities());
+	m_cell1->averageTracerDensities(m_tracerDensities, m_cell1->getTracerDensities(), m_cell2->getTracerDensities());*/
 }
 
 void Link::getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse)
 {
 	sourceTracerDensities=m_tracerDensities;
 	transferVolume=m_transferVolume;
-	parallelImpulse=m_pressureImpulse;
-	perpendicularImpulse=UNG_FLT(0.0);
+	parallelImpulse=m_parallelImpulse;
+	perpendicularImpulse=m_perpendicularImpulse;
 }
 
+InviscidLink::InviscidLink(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2)
+	:Link(cell1, cell2, area, speed1, speed2, inviscidLinkUpdate)
+{
+}
 
-BoundaryLink::BoundaryLink(Cell *cell, UNG_FLT area, const UNG_FLT *speed)
-	:Link(cell, nullptr, area, speed, nullptr)
+BoundaryLink::BoundaryLink(Cell *cell, UNG_FLT area, const UNG_FLT *speed, void (*updateFunc)( void *, UNG_FLT ))
+	:Link(cell, nullptr, area, speed, nullptr, updateFunc)
 {
 	size_t size=cell->getTracerDensitySize();
 	char* dest=(char*)m_tracerDensities;
@@ -44,18 +65,13 @@ BoundaryLink::BoundaryLink(Cell *cell, UNG_FLT area, const UNG_FLT *speed)
 }
 
 SolidBoundaryLink::SolidBoundaryLink(Cell *cell, UNG_FLT area, const UNG_FLT *speed)
-	:BoundaryLink(cell, area, speed)
+	:BoundaryLink(cell, area, speed, solidBoundaryLinkUpdate)
 {
+	m_transferVolume=0;
+	m_parallelImpulse=0;
+	m_perpendicularImpulse=0;
 }
 
-void SolidBoundaryLink::update(UNG_FLT timeStep)
-{
-}
-
-void SolidBoundaryLink::getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse)
-{
-	sourceTracerDensities=m_tracerDensities;
-	transferVolume=UNG_FLT(0.0);
-	parallelImpulse=UNG_FLT(0.0);
-	perpendicularImpulse=UNG_FLT(0.0);
-}
+//void SolidBoundaryLink::update(UNG_FLT timeStep)
+//{
+//}
--- ung/link.h
+++ ung/link.h
@@ -5,41 +5,55 @@
 
 class Cell;
 
+void inviscidLinkUpdate( void *linkVoid, UNG_FLT timestep);
+void solidBoundaryLinkUpdate( void *solidBoundaryLink, UNG_FLT timestep);
+
 class Link
 {
 public:
-	Link(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2);
+	Link(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2, void (*updateFunc)( void *, UNG_FLT ));
 	~Link();
-	virtual void update(UNG_FLT timeStep);
-	virtual void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse);
+	void update(UNG_FLT timeStep);
+	void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse);
 protected:
 	UNG_FLT m_area;
 	Cell *m_cell1;
 	Cell *m_cell2;
 	const UNG_FLT * m_speed1; 
 	const UNG_FLT * m_speed2;
-	UNG_FLT m_pressureImpulse;
 	UNG_FLT m_transferVolume;
 	void *m_tracerDensities;
+
+	UNG_FLT m_perpendicularImpulse;
+	UNG_FLT m_parallelImpulse;
+	void (*const m_updateFunc)( void *, UNG_FLT );
 
 
 	Link();
 };
 
+class InviscidLink : public Link
+{
+	friend void inviscidLinkUpdate(void *, UNG_FLT);
+public:
+	InviscidLink(Cell *cell1, Cell*cell2, UNG_FLT area, const UNG_FLT *speed1, const UNG_FLT *speed2);
+};
+
 class BoundaryLink: public Link
 {
 public:
-	BoundaryLink(Cell *cell1, UNG_FLT area, const UNG_FLT *speed);
-	virtual void update(UNG_FLT timeStep)=0;
-	virtual void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse)=0;
+	BoundaryLink(Cell *cell1, UNG_FLT area, const UNG_FLT *speed, void (*updateFunc)( void *, UNG_FLT ));
+	//virtual void update(UNG_FLT timeStep)=0;
+	//virtual void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse)=0;
 };
 
 class SolidBoundaryLink: public BoundaryLink
 {
+	friend void solidBoundaryLinkUpdate(void *, UNG_FLT);
 public:
 	SolidBoundaryLink(Cell *cell1, UNG_FLT area, const UNG_FLT *speed);
-	virtual void update(UNG_FLT timeStep);
-	virtual void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse);
+	//virtual void update(UNG_FLT timeStep);
+	//virtual void getLinkProperties( const void *&sourceTracerDensities, UNG_FLT &transferVolume, UNG_FLT &parallelImpulse, UNG_FLT &perpendicularImpulse);
 
 };
 #endif
